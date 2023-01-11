1d5d32cc4a1388c535808bb50e621bf7d9a0df87
hozlucas28 <hozlucas28@gmail.com>
2023-01-11 15:35:01 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+// Importaciones React
+import { Component } from 'react';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img
-					src={logo}
-					className="App-logo"
-					alt="logo"
+// Componentes
+import Products from './components/Products';
+import Layout from './components/Layout';
+import Title from './components/Title';
+import Navbar from './components/Navbar';
+
+class App extends Component {
+	state = {
+		cart: [
+			// {
+			// 	name: 'Tomate',
+			// 	price: 1500,
+			// 	image: './products/tomatoes.jpg',
+			// 	amount: 1
+			// }
+		],
+
+		products: [
+			{
+				name: 'Tomate',
+				price: 1500,
+				image: './products/tomatoes.jpg'
+			},
+			{
+				name: 'Arvejas',
+				price: 2500,
+				image: './products/vetch.jpg'
+			},
+			{
+				name: 'Lechuga',
+				price: 500,
+				image: './products/lettuce.jpg'
+			}
+		],
+
+		isCartVisible: false
+	};
+
+	// Añadir productos al carrito
+	addToCart = (product) => {
+		const { cart } = this.state;
+
+		if (cart.find((x) => x.name === product.name)) {
+			const newCart = cart.map((x) => (x.name === product.name ? { ...x, amount: x.amount + 1 } : x));
+			return this.setState({ cart: newCart });
+		}
+
+		return this.setState({
+			cart: this.state.cart.concat({
+				...product,
+				amount: 1
+			})
+		});
+	};
+
+	// Mostrar carrito
+	showCart = () => {
+		if (!this.state.cart.length) {
+			return;
+		}
+
+		this.setState({ isCartVisible: !this.state.isCartVisible });
+	};
+
+	render() {
+		return (
+			<div>
+				<Navbar
+					cart={this.state.cart}
+					showCart={this.showCart}
+					isCartVisible={this.state.isCartVisible}
 				/>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+				<Layout>
+					<Title>Tienda</Title>
+					<Products
+						addToCart={this.addToCart}
+						products={this.state.products}
+					/>
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
